66813bfbebdb806647d8b22bdcc6630a51dde44a
Shaju Mohammed <shajumohamed@yahoo.com>
2019-07-16 10:54:24 +0000

    hello

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const WebSocketServer = require('ws').Server,
   http = require('http'),
   app = express(),
   fs = require('fs');
+  CLIENTS=[];
 
 var wss = null, sslSrv = null;
 
   app.use(express.static('public'));
+
 
   app.use(function(req, res, next) {
   if(req.headers['x-forwarded-proto']==='http') {
@@ -14,7 +16,8 @@ var wss = null, sslSrv = null;
   }
   next();
 });
-  sslSrv = http.createServer(app).listen(process.env.PORT||443);
+
+  sslSrv = http.createServer(app).listen(process.env.PORT||8080);
   console.log("The HTTPS server is up and running");
 
 // create the WebSocket server
@@ -23,6 +26,7 @@ console.log("WebSocket Secure server is up and running.");
 
   /** successful connection */
 wss.on('connection', function (client) {
+	CLIENTS.push(client);
   console.log("A new WebSocket client was connected.");
   /** incomming message */
   client.on('message', function (message) {
@@ -31,13 +35,18 @@ wss.on('connection', function (client) {
   });
 });
 
+wss.on('error',function(err){
+
+	console.log(err)});
+
+
 // broadcasting the message to all WebSocket clients.
 wss.broadcast = function (data, exclude) {
-  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
+  var i = 0, n = CLIENTS.length , client = null;
   if (n < 1) return;
   console.log("Broadcasting message to all " + n + " WebSocket clients.");
   for (; i < n; i++) {
-    client = this.clients[i];
+    client = CLIENTS[i];
     // don't send the message to the sender...
     if (client === exclude) continue;
     if (client.readyState === client.OPEN) client.send(data);
