8b6575e650e0680ee7dc8f4a1f37e8f120cb85e4
Shaju Mohammed <shajumohamed@yahoo.com>
2019-07-29 10:35:16 +0000

    changed turn

--- public/webrtc.js
+++ public/webrtc.js
@@ -24,13 +24,10 @@ var peerConn = null,
 //   };
 
 peerConnCfg={
-    iceServers:{"urls": 
-    [
-    "turn:13.250.13.83:3478?transport=udp"
-    ],
+    iceServers:[{"url": "turn:13.250.13.83:3478?transport=udp",
     "username": "YzYNCouZM1mhqhmseWk6",
     "credential": "YzYNCouZM1mhqhmseWk6"
-    }
+    }]
 }
 
     
